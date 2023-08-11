5b9be3ad45e9c422241e1207b7a9575869709953
Eulagly <137733009+Eulagly@users.noreply.github.com>
2023-08-11 11:21:04 -0400

    Update run.py

--- my-mobile-app/backend/run.py
+++ my-mobile-app/backend/run.py
@@ -2,7 +2,3 @@ from .app import create_app
 
 # Create the Flask app instance
 app = create_app()
-
-# Run the development server
-def run(*args, **kwargs):
-    app.run(debug=True, host='0.0.0.0', port=5000)
